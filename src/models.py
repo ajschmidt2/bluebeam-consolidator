56bd36aa29b6e6bcdded6ab7a2fd64b008393511
ajschmidt2 <adam.james.schmidt@gmail.com>
2026-01-21 13:35:43 -0500

    Add table_args to Project model for extension

--- src/models.py
+++ src/models.py
@@ -7,6 +7,7 @@ from sqlmodel import SQLModel, Field
 
 
 class Project(SQLModel, table=True):
+    __table_args__ = {"extend_existing": True}
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     client: Optional[str] = None
